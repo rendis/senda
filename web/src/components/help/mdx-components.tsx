95683770a984fd017170eda904e4372fab3fa1cd
Reynaldo Endis Peña <reynaldo.endis@gmail.com>
2026-04-02 22:57:49 -0300

    fix(help): expand API key endpoint reference

--- web/src/components/help/mdx-components.tsx
+++ web/src/components/help/mdx-components.tsx
@@ -1,8 +1,10 @@
 import type { MDXComponents } from "mdx/types";
+import { ApiEndpointReference } from "@/components/help/api-endpoint-reference";
 import { HelpLink } from "@/components/help/help-link";
 
 export function getHelpMDXComponents(): MDXComponents {
   return {
     a: HelpLink,
+    ApiEndpointReference,
   };
 }
